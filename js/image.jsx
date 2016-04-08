4d09a0b8f7c097b77f9a38c470b23171b5fb7f97
David Sattler <dsmine1737@gmail.com>
2016-04-08 20:38:52 +1000

    html_includes

---
+++ js/image.jsx
@@ -0,0 +1,48 @@
+var RoundedImage = React.createClass({
+    render: function(){
+        var style = {
+            width: this.props.size[0] + "px",
+            height: this.props.size[1] + "px"
+        };
+        return(
+            <ReactBootstrap.Image style={style} src="https://react-bootstrap.github.io/assets/thumbnail.png" rounded />
+        );
+    }
+});
+
+var CircleImage = React.createClass({
+    render: function(){
+        var style = {
+            width: this.props.size[0] + "px",
+            height: this.props.size[1] + "px"
+        };
+        return(
+            <ReactBootstrap.Image style={style} src="https://react-bootstrap.github.io/assets/thumbnail.png" circle />
+        );
+    }
+});
+
+var ThumbnailImage = React.createClass({
+    render: function(){
+        var style = {
+            width: this.props.size[0] + "px",
+            height: this.props.size[1] + "px"
+        };
+        return(
+            <ReactBootstrap.Image style={style} src="https://react-bootstrap.github.io/assets/thumbnail.png" thumbnail />
+        );
+    }
+});
+
+function CreateCircleImage(width, height, targetID)
+{
+    ReactDOM.render(<CircleImage size={[width, height]} />, document.getElementById(targetID));
+}
+
+function CreateRoundedImage(width, height, targetID){
+    ReactDOM.render(<RoundedImage size={[width, height]} />, document.getElementById(targetID));
+}
+
+function CreateThumbnailImage(width, height, targetID){
+    ReactDOM.render(<ThumbnailImage size={[width, height]} />, document.getElementById(targetID));
+}
