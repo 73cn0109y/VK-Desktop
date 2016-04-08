a5b8905ad24ce314cdaa39c82a1a3deef905ce90
David Sattler <73cn0109y@users.noreply.github.com>
2016-04-08 18:20:10 +1000

    fixed

---
+++ js/topbar.jsx
@@ -0,0 +1,44 @@
+var TopBarTabButton = React.createClass({
+    getInitialState: function() {
+        return {isSelected: false};
+    },
+    onClicked: function() {
+        this.setState({isSelected: true});
+        this.props.callbackParent(this.props.name);
+    },
+    render: function() {
+        var selectorClassName = classNames(
+            "topbar-tab-selector",
+            { "selected": (this.props.selected == this.props.name) }
+        );
+        return (
+            <div className="topbar-tab" onClick={this.onClicked}>
+                {this.props.name}
+                <div className={selectorClassName}/>
+            </div>
+        );
+    }
+});
+
+var TabBar = React.createClass({
+    getInitialState: function() {
+        return { selected: this.props.items[0] }
+    },
+    handleClick: function(e) {
+        this.setState({selected: e});
+    },
+    render: function() {
+        var self = this;
+        return (
+            <div>
+                { this.props.items.map(function(item, i){
+                    return (
+                        <TopBarTabButton name={item} key={i} selected={self.state.selected} callbackParent={self.handleClick} />
+                    );
+                })}
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<TabBar items={["News", "Update", "Comments"]} />, document.getElementById('topbar'));
