b3cb04c71aa856d7e3380ca49fa9b1aab6a89eb2
David Sattler <dsmine1737@gmail.com>
2016-04-10 00:54:50 +1000

    news page layout completed

--- html_includes/js/news_main.jsx
+++ html_includes/js/news_main.jsx
@@ -1,3 +1,5 @@
+var itemHeights = new Array();
+
 var NewsData = function (_id, _title, _content, _image)
 {
     this.ID = _id;
@@ -7,24 +9,19 @@ var NewsData = function (_id, _title, _content, _image)
 }
 
 var NewsItem = React.createClass({
-    getInitialState: function(){
-        return { height: 0 }
-    },
     componentDidMount: function(){
-        //console.log(ReactDOM.findDOMNode(this).offsetWidth);
-        this.setState({ height: -(ReactDOM.findDOMNode(this).offsetHeight) });
+        itemHeights.push(new Array(
+            ReactDOM.findDOMNode(this),
+            ReactDOM.findDOMNode(this).offsetHeight)
+        );
     },
     render: function(){
-        var style = {
-            left: this.props.left,
-            top: (this.props.data.ID == 0 ? 0 : this.state.height + 10)
-        };
         var headerSplitClass = classNames(
             "hasText",
             { "visible": (this.props.data.Content != null) }
         );
         return (
-            <div id={this.props.data.ID} className="news-item" style={style}>
+            <div id={this.props.data.ID} className="news-item">
                 <div className="header">
                     <div className="user-picture">
                         <CircleImage size={[49, 49]} />
@@ -52,18 +49,11 @@ var NewsItem = React.createClass({
 
 var NewsPage = React.createClass({
     render: function(){
-        var left = "5%";
         return (
             <div id="news-page">
                 { this.props.items.map(function(item, i){
-                    if(i % 2 == 0 && i != 0){
-                        left = "5%";
-                    }
-                    else if(i != 0){
-                        left = "50%";
-                    }
                     return (
-                        <NewsItem data={item} key={i} left={left} />
+                        <NewsItem data={item} key={i} />
                     );
                 })}
             </div>
@@ -74,12 +64,54 @@ var NewsPage = React.createClass({
 function SampleNews()
 {
     var news = new Array();
-    news.push(new NewsData(0, "Headhunterz shared a link.", "My new track Lift Me Up is finally out.. I must admit.. I'm very proud of this one. Thanks for all your support!!", null));
-    news.push(new NewsData(1, "Akon", "Let's get it started!", null));
-    news.push(new NewsData(2, "Akon", "Let's get it started!", null));
-    news.push(new NewsData(3, "Headhunterz shared a link.", "My new track Lift Me Up is finally out.. I must admit.. I'm very proud of this one. Thanks for all your support!!", "https://external-syd1-1.xx.fbcdn.net/safe_image.php?d=AQDCuo4QGJ9WyDyW&w=470&h=246&url=http%3A%2F%2Fcdn-uza9x79xqk.s3.amazonaws.com%2Fsites%2F24%2F2016%2F04%2F07184347%2Fbeps2-2.jpg&cfs=1&upscale=1&sx=1&sy=0&sw=1198&sh=627&ext=png2jpg"));
+    news.push(new NewsData(0, "Item 0", "My new track Lift Me Up is finally out.. I must admit.. I'm very proud of this one. Thanks for all your support!!", null));
+    news.push(new NewsData(1, "Item 1", "Let's get it started!", null));
+    news.push(new NewsData(2, "Item 2", "Let's get it started!", null));
+    news.push(new NewsData(3, "Item 3", "My new track Lift Me Up is finally out.. I must admit.. I'm very proud of this one. Thanks for all your support!!", "https://external-syd1-1.xx.fbcdn.net/safe_image.php?d=AQDCuo4QGJ9WyDyW&w=470&h=246&url=http%3A%2F%2Fcdn-uza9x79xqk.s3.amazonaws.com%2Fsites%2F24%2F2016%2F04%2F07184347%2Fbeps2-2.jpg&cfs=1&upscale=1&sx=1&sy=0&sw=1198&sh=627&ext=png2jpg"));
+    news.push(new NewsData(4, "Item 4", "My new track Lift Me Up is finally out.. I must admit.. I'm very proud of this one. Thanks for all your support!!", null));
+    news.push(new NewsData(5, "Item 5", "Let's get it started!", null));
+    news.push(new NewsData(6, "Item 6", "Let's get it started!", null));
+    news.push(new NewsData(7, "Item 7", "My new track Lift Me Up is finally out.. I must admit.. I'm very proud of this one. Thanks for all your support!!", null));
 
     ReactDOM.render(<NewsPage items={news} />, document.getElementById("news-board"));
+
+    var colPositions = new Array(10, 10);
+
+    for(var i = 0; i < itemHeights.length; i++)
+    {
+        if(colPositions[0] <= colPositions[1]) {
+            itemHeights[i][0].style.left = "5%";
+            itemHeights[i][0].style.top = colPositions[0];
+            colPositions[0] += itemHeights[i][1] + 10;
+        }
+        else {
+            itemHeights[i][0].style.left = "50%";
+            itemHeights[i][0].style.top = colPositions[1];
+            colPositions[1] += itemHeights[i][1] + 10;
+        }
+        /*if(i % 2 == 0)
+        {
+            itemHeights[i][0].style.left = "5%";
+            itemHeights[i][0].style.top = colPositions[0];
+            colPositions[0] += itemHeights[i][1] + 10;
+        }
+        else
+        {
+            if(colPositions[0] < colPositions[1]) {
+                itemHeights[i][0].style.left = "5%";
+                itemHeights[i][0].style.top = colPositions[0];
+                colPositions[0] += itemHeights[i][1] + 10;
+            }
+            else {
+                itemHeights[i][0].style.left = "50%";
+                itemHeights[i][0].style.top = colPositions[1];
+                colPositions[1] += itemHeights[i][1] + 10;
+            }
+
+            /*itemHeights[i][0].style.top = colPositions[1];
+            colPositions[1] += itemHeights[i][1] + 10;
+        }*/
+    }
 }
 
 SampleNews();
